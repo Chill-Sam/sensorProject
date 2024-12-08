06749fb295670db36aa089c78e7580b63e00db1c
Jonathan Wahrenberg <jonathan.wahrenberg@hitachigymnasiet.se>
2024-12-08 12:48:22 +0100

    Added sensitive information

---
+++ web/public/sensitive.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-q4cAoA8XO2VsfP1yfBSTnhE0yMGEKFo",
+    authDomain: "school-sensor-2ddce.firebaseapp.com",
+    databaseURL:
+        "https://school-sensor-2ddce-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "school-sensor-2ddce",
+    storageBucket: "school-sensor-2ddce.firebasestorage.app",
+    messagingSenderId: "555790047594",
+    appId: "1:555790047594:web:daebd79b8eb470b0db2d13",
+};
